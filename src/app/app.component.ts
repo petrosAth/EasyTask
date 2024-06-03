c4c98aec7472ed2f1837d90bc2bd474a645f2279
Petros Athanasoulis <develop@athanasoulis.me>
2024-06-03 17:28:08 +0300

    Revert "Using ngFor & ngIf"
    This reverts commit 7ff9d51bcabd754421220b6977f5d87201acdb6c.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,5 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { NgFor, NgIf } from '@angular/common';
-
 import { HeaderComponent } from "./header/header.component";
 import { UserComponent } from "./user/user.component";
 import { DUMMY_USERS } from "./dummy-users";
@@ -10,7 +8,7 @@ import { TasksComponent } from './tasks/tasks.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, UserComponent, TasksComponent, NgFor, NgIf],
+  imports: [RouterOutlet, HeaderComponent, UserComponent, TasksComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
