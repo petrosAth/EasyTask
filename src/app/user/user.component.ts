74e769cd8281a825a4f770c7e5a253c942314385
Petros Athanasoulis <develop@athanasoulis.me>
2024-06-03 16:30:15 +0300

    Create a Configurable Component

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -7,9 +7,9 @@ import { Component, Input, Output, EventEmitter } from '@angular/core';
   styleUrl: './user.component.css'
 })
 export class UserComponent {
-  @Input({required: true}) id!: string;
-  @Input({required: true}) avatar!: string;
-  @Input({required: true}) name!: string;
+  @Input({ required: true }) id!: string;
+  @Input({ required: true }) avatar!: string;
+  @Input({ required: true }) name!: string;
   @Output() select = new EventEmitter<string>();
 
   get imagePath() {
